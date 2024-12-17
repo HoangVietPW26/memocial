6829a2c11aa3c083dc5f8bd87745598f4ca18563
HoangVietPW26 <nhviet26@gmail.com>
2024-12-17 11:28:42 +0000

    update Form component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-import React from "react";
+import { getPosts } from "./actions/posts.js";
+import Posts from "./components/Posts/Posts.jsx";
+import Post from "./components/Posts/Post/Post.jsx";
+import Form from "./components/Form/Form.jsx";
+import memories from "./images/memories.png";
+import useStyles from "./styles.js";
 
 const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height={120}
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts></Posts>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form></Form>
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+
+import "./index.css";
+import App from "./App.jsx";
+import reducers from "./reducers";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
