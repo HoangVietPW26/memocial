653abb7e7328888804fc0fca4b1c9955e412e616
HoangVietPW26 <nhviet26@gmail.com>
2024-12-18 16:22:21 +0000

    deploy test

--- server/index.js
+++ server/index.js
@@ -18,7 +18,8 @@ app.use('/posts', postRoutes)
 app.use("/", homeRoute)
 
 const PORT = process.env.PORT || 5000
-console.log(PORT)
+const URL = process.env.CONNECTION_URL
+console.log(URL)
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.messege))
